1ef734100decc0c136e009a80067a3d4308249bf
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-07-01 08:42:34 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ fn parse_commands(input: String) {
         return;
     }
     let input = input.trim();
+    if input.starts_with("exit") {
+        match input.split_once(' ') {
+            Some((_, code)) => {
+                let code = code.parse::<i32>().unwrap();
+                std::process::exit(code);
+            }
+            None => println!("Invalide command {input}"),
+        }
+        std::process::exit(0);
+    }
     println!("{input}: command not found");
 }
 
