3970ea8669c9aae02a7ac14816bcc607160123e9
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-07-01 09:04:59 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+static COMMANDS: &[&str] = &["type", "echo", "exit"];
+
 fn parse_commands(input: String) {
     let input = input.trim();
     let mut split = input.split(' ');
@@ -11,6 +13,15 @@ fn parse_commands(input: String) {
         }
         Some("echo") => {
             println!("{}", split.collect::<Vec<_>>().join(" "));
+        }
+        Some("type") => {
+            if let Some(command) = split.next() {
+                if COMMANDS.contains(&command) {
+                    println!("{command} is a shell builtin");
+                } else {
+                    println!("{command}: not found");
+                }
+            }
         }
         Some(x) => {
             println!("{x}: command not found");
